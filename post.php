c99c1d3e9442208dc5a564159083c6e1a4f507ab
hereiam123 <bdemaio1@gmail.com>
2017-05-22 07:05:56 -0400

    setup page layouts with dummy content

---
+++ post.php
@@ -0,0 +1,15 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="blog-post">
+    <h2 class="blog-post-title">PHP conference 2017</h2>
+    <p class="blog-post-meta">January 1, 2017 by <a href="#">Mark</a></p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquam convallis lorem et lacinia. Sed faucibus nibh vel eleifend vestibulum. Sed odio risus, dictum ac justo ut, rhoncus volutpat dolor. Maecenas maximus pretium fermentum. Fusce felis augue, tristique sed orci porta, porttitor accumsan nisl. In pharetra commodo felis quis dictum. Duis hendrerit ultrices fringilla. Aliquam erat volutpat. Nulla id libero est. Nulla efficitur nisl sit amet purus elementum dignissim. Donec ligula lectus, venenatis et rutrum a, tempus eu est. Pellentesque blandit accumsan nunc ut laoreet. Donec ut egestas arcu. Praesent finibus sit amet quam eu ornare. Nunc eros nibh, aliquet at ligula sit amet, venenatis sagittis nibh. Suspendisse facilisis quis sem eu porttitor.
+
+        Proin rhoncus tellus at nisl tempus sollicitudin. Etiam in ante nisi. Fusce accumsan leo enim, vitae fermentum purus laoreet eget. Sed tincidunt leo id elit rutrum, eget malesuada sem porttitor. Phasellus ut ultricies ex. Duis elementum mollis metus, vel lacinia ex porttitor eget. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas interdum suscipit eros sit amet cursus.
+
+        Integer rutrum est et orci convallis posuere. Nam id finibus justo. Aliquam placerat magna et magna ultrices volutpat. Nunc fermentum dui eget elementum aliquam. Pellentesque eget lorem sit amet velit tempor ultricies ac sit amet nibh. In vestibulum, massa vitae dictum pulvinar, orci orci iaculis mi, faucibus tristique ipsum justo non elit. Quisque vehicula turpis id mi interdum, a rutrum leo dapibus. Nullam congue eros vitae iaculis molestie. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris tempus arcu eu iaculis laoreet. Sed facilisis convallis sollicitudin. Suspendisse consectetur sodales dapibus. Nam laoreet velit neque.
+    </p>
+</div><!-- /.blog-post -->
+
+<?php include 'includes/footer.php'; ?>
---
+++ posts.php
@@ -0,0 +1,30 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="blog-post">
+    <h2 class="blog-post-title">PHP conference 2017</h2>
+    <p class="blog-post-meta">January 1, 2017 by <a href="#">Mark</a></p>
+    <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquam convallis lorem et lacinia. Sed faucibus nibh vel eleifend vestibulum. Sed odio risus, dictum ac justo ut, rhoncus volutpat dolor. Maecenas maximus pretium fermentum. Fusce felis augue, tristique sed orci porta, porttitor accumsan nisl. In pharetra commodo felis quis dictum. Duis hendrerit ultrices fringilla. Aliquam erat volutpat. Nulla id libero est. Nulla efficitur nisl sit amet purus elementum dignissim. Donec ligula lectus, venenatis et rutrum a, tempus eu est. Pellentesque blandit accumsan nunc ut laoreet. Donec ut egestas arcu. Praesent finibus sit amet quam eu ornare. Nunc eros nibh, aliquet at ligula sit amet, venenatis sagittis nibh. Suspendisse facilisis quis sem eu porttitor.
+    </p>
+    <a class="readmore" href="post.php?id=1">Read More</a>
+</div><!-- /.blog-post -->
+
+<div class="blog-post">
+  <h2 class="blog-post-title">PHP International Fun Day</h2>
+  <p class="blog-post-meta">February 13, 2017 by <a href="#">Mark</a></p>
+  <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquam convallis lorem et lacinia. Sed faucibus nibh vel eleifend vestibulum. Sed odio risus, dictum ac justo ut, rhoncus volutpat dolor. Maecenas maximus pretium fermentum. Fusce felis augue, tristique sed orci porta, porttitor accumsan nisl. In pharetra commodo felis quis dictum. Duis hendrerit ultrices fringilla. Aliquam erat volutpat. Nulla id libero est. Nulla efficitur nisl sit amet purus elementum dignissim. Donec ligula lectus, venenatis et rutrum a, tempus eu est. Pellentesque blandit accumsan nunc ut laoreet. Donec ut egestas arcu. Praesent finibus sit amet quam eu ornare. Nunc eros nibh, aliquet at ligula sit amet, venenatis sagittis nibh. Suspendisse facilisis quis sem eu porttitor.
+  </p>
+  <a class="readmore" href="post.php?id=2">Read More</a>
+</div><!-- /.blog-post -->
+
+<div class="blog-post">
+    <h2 class="blog-post-title">PHP SQL Query Questions</h2>
+    <p class="blog-post-meta">March 24, 2017 by <a href="#">Mark</a></p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquam convallis lorem et lacinia. Sed faucibus nibh vel eleifend vestibulum. Sed odio risus, dictum ac justo ut, rhoncus volutpat dolor. Maecenas maximus pretium fermentum. Fusce felis augue, tristique sed orci porta, porttitor accumsan nisl. In pharetra commodo felis quis dictum. Duis hendrerit ultrices fringilla. Aliquam erat volutpat. Nulla id libero est. Nulla efficitur nisl sit amet purus elementum dignissim. Donec ligula lectus, venenatis et rutrum a, tempus eu est. Pellentesque blandit accumsan nunc ut laoreet. Donec ut egestas arcu. Praesent finibus sit amet quam eu ornare. Nunc eros nibh, aliquet at ligula sit amet, venenatis sagittis nibh. Suspendisse facilisis quis sem eu porttitor.
+    </p>
+    <a class="readmore" href="post.php?id=3">Read More</a>
+</div><!-- /.blog-post -->
+
+<?php include 'includes/footer.php'; ?>
