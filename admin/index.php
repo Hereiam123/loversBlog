dcaf7ed55a11bc23edbabb94a90936a208026558
hereiam123 <bdemaio1@gmail.com>
2017-05-23 12:58:26 -0400

    connected databse to dashboard to view posts, added join query to view category names

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,18 @@
 <?php include 'includes/header.php' ; ?>
+<?php
+    //Create DB Object
+    $db=new Database;
+
+    //Make query
+    $query="SELECT posts.*, categories.name FROM posts
+    INNER JOIN categories ON posts.category = categories.id";
+
+    //Run query
+    $posts = $db->select($query);
+?>
 
 <table class="table table-striped">
+
     <tr>
         <th>Post ID#</th>
         <th>Post Title</th>
@@ -8,13 +20,17 @@
         <th>Author</th>
         <th>Date</th>
     </tr>
+
+    <?php while($row=$posts->fetch_assoc()) : ?>
     <tr>
-        <td></th>
-        <td></th>
-        <td></th>
-        <td></th>
-        <td></th>
+        <td><?php echo $row['id']; ?></th>
+        <td><a href="edit_post.php?id=<?php echo $row['id'];?>"><?php echo $row['title']; ?></a></th>
+        <td><?php echo $row['name']; ?></th>
+        <td><?php echo $row['author']; ?></th>
+        <td><?php echo formatDate($row['date']); ?></th>
     </tr>
+    <?php endwhile; ?>
+
 </table>
 
 <table class="table table-striped">
