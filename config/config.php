724c25be3a2e18a1d49552dbc1daadc1668bd711
hereiam123 <bdemaio1@gmail.com>
2017-05-23 08:37:35 -0400

    categories menu dynamically loading and about text placed in config file

--- config/config.php
+++ config/config.php
@@ -3,3 +3,5 @@
     define('DB_USER','root');
     define('DB_PASS','');
     define('DB_NAME','phploversblog');
+
+    $site_description = 'Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.';
